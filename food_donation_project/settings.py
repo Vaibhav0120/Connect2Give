07cc3182869ef58e8d1bcf069fff75e160863848
vaibhav0120 <vaibhavsh0120@gmail.com>
2025-10-05 12:18:02 +0530

    Testing on Network Devices Complete

--- food_donation_project/settings.py
+++ food_donation_project/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.167' , '127.0.0.1', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
